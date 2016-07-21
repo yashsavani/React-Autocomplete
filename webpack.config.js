b04e9ef9a4203cefffd1079ee55607bbd93e9134
yashsavani <yash.savani@gmail.com>
2016-07-21 23:45:49 +0530

    production build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'inline-source-map',
-  entry: [
+  devtool: debug ? 'inline-source-map' : null,
+  entry: debug ? [
     'webpack-hot-middleware/client',
     './client/client.js'
-  ],
+  ] : './client/client.js',
   output: {
     path: require('path').resolve('./dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
-  plugins: [
+  plugins: debug ? [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
+  ] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015', 'react-hmre']
+          presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
         }
       }
     ]
